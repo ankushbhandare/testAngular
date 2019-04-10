b50b4ba587bca9adc4180dba070dac9739a16f08
ankushbhandare <bhandare_ankush@rediffmail.com>
2019-04-09 17:10:44 -0700

    Added code as per problem statement in html and css files

--- basicApp/src/app/app.component.ts
+++ basicApp/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'basicApp';
+  counter : number;
+  ngOnInit(){
+    this.counter=0;
+  }
+  incrementCounter()
+  {
+    this.counter++;
+  }
 }
+
+
